509700662369f3ac9d066fc6dd4ef044f63c8a11
Tedd <91820662+teddbeef@users.noreply.github.com>
2022-06-18 23:33:49 +0700

    Chapter 6.3 - Calling Methods and Jumping to Source

--- selenium-tutorial-master/src/test/java/taudemo2/SeleniumTest.java
+++ selenium-tutorial-master/src/test/java/taudemo2/SeleniumTest.java
@@ -1,4 +1,30 @@
 package taudemo2;
 
+import browser.BrowserGetter;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
 public class SeleniumTest {
+    private BrowserGetter browserGetter = new BrowserGetter();
+    private WebDriver driver;
+
+    @BeforeAll
+    public void beforAll() {
+        driver = browserGetter.getChromeDriver();
+
+    }
+
+    @AfterAll
+    public void afterAll() {
+        driver.quit();
+    }
+
+    @Test
+    public void openThePageAndCheckTheTitle() {
+        String expectedTitle = "Example title";
+
+
+    }
 }
