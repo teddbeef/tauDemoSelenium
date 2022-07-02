f050dc4e6726216ef918dc086a965ac2c218595b
Tedd <91820662+teddbeef@users.noreply.github.com>
2022-07-02 23:31:18 +0700

    Update code

--- selenium-tutorial-master/src/test/java/taudemo2/SeleniumTest.java
+++ selenium-tutorial-master/src/test/java/taudemo2/SeleniumTest.java
@@ -1,19 +1,20 @@
 package taudemo2;
 
 import browser.BrowserGetter;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+@TestInstance(PER_CLASS)
 public class SeleniumTest {
-    private BrowserGetter browserGetter = new BrowserGetter();
+    private final BrowserGetter browserGetter = new BrowserGetter();
     private WebDriver driver;
 
     @BeforeAll
     public void beforAll() {
         driver = browserGetter.getChromeDriver();
-
     }
 
     @AfterAll
@@ -21,10 +22,20 @@ public class SeleniumTest {
         driver.quit();
     }
 
+
+    //Shift + F6 is rename the function
     @Test
-    public void openThePageAndCheckTheTitle() {
-        String expectedTitle = "Example title";
-
-
+    public void openTheComPageAndCheckTheTitle() {
+        String expectedComTitle = "Example title";
+        driver.get("https://www.example.com");
+        assertEquals(expectedComTitle, driver.getTitle());
+        Assertions.assertEquals(1, 2);
+    }
+    @Test
+    public void openTheOrgPageAndCheckTheTitle() {
+        String expectedOrgTitle = "Example title";
+        driver.get("https://www.example.org");
+        assertEquals(expectedOrgTitle, driver.getTitle());
+        Assertions.assertEquals(1, 2);
     }
 }
